e66bf495cf917041124959ad1e95239405c19cf5
Menomcking <patrik8870@gmail.com>
2022-09-27 12:14:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+let music = {
+    title : "",
+    leng : 0,
+}
+class Music {
+    constructor(title, leng){
+        this.title = title;
+        that.leng = leng;
+    }
+    get 
+}
+const tracks = [];
+addtrack(){
+
+}
